c1eac509763a7ea095d38d6035b5e375f7c58382
HansSima <SimaHonza@seznam.cz>
2025-06-24 20:32:53 +0200

    init commit

---
+++ src/main/java/SelTestTutorialPointCheck.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+
+
+public class SelTestTutorialPointCheck {
+
+    private WebDriver ovladac;
+
+    @BeforeEach
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\hanss\\Desktop\\Software Development Academy\\Kody\\SeleniumSkillmea2\\src\\main\\resources\\chromedriver.exe");
+
+
+    }
+
+}
