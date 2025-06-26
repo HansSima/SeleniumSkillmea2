6c0c9ec4d17af3266991f14a6194c9aa5cd3565c
HansSima <SimaHonza@seznam.cz>
2025-06-26 18:58:54 +0200

    commit

--- src/main/java/SelTestTutorialPointCheck.java
+++ src/main/java/SelTestTutorialPointCheck.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class SelTestTutorialPointCheck {
@@ -15,6 +17,9 @@ public class SelTestTutorialPointCheck {
     public void setUp(){
         //System.setProperty("webdriver.chrome.driver", "C:\\Users\\hanss\\Desktop\\Software Development Academy\\Kody\\SeleniumSkillmea2\\src\\main\\resources\\chromedriver.exe");
         //ovladac = new ChromeDriver();
+
+        //System.setProperty("webdriver.gecko.driver", "C:\\Users\\hanss\\Desktop\\Software Development Academy\\Kody\\BddZ4089\\src\\main\\resources\\geckodriver.exe");
+        //ovladac = new FirefoxDriver();
 
         WebDriverManager.firefoxdriver().setup();
         ovladac = new ChromeDriver();
@@ -34,6 +39,8 @@ public class SelTestTutorialPointCheck {
         //Kliknuti na checkboxy
         ovladac.findElement(By.id("c_bf_1")).click();
         ovladac.findElement(By.id("c_bf_2")).click();
+
+        assertTrue(ovladac.findElement(By.id("c_bs_1")).isSelected());
 
     }
 
